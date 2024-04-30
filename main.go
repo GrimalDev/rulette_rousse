4a00c4f9be3ed15410a5a4902eeb3cef212cd4c8
ratataque <ewanrimmely@outlook.fr>
2024-04-30 11:01:43 +0200

    template init

--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
 
-import "github.com/labstack/echo/v4"
+import (
+	"text/template"
 
-func main() {
-	e := echo.new()
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Templates struct {
+	templates *template.Template
 }
 
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+}
