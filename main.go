0680e31014df5b531af9060d3ee0ccc3911bd4c9
GrimalDev <grimaldev@grimaldev.local>
2024-04-30 14:44:36 +0200

    exemple code

--- main.go
+++ main.go
@@ -1,13 +1,44 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"html/template"
+	"io"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
-func main() {
-	for {
-		fmt.Println("TESTTT")
-		time.Sleep(time.Second * 3)
+type Templates struct {
+	tmpl *template.Template
+}
+
+func newTemplate() *Templates {
+	return &Templates{
+		tmpl: template.Must(template.ParseGlob("views/*.html")),
 	}
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.tmpl.ExecuteTemplate(w, name, data)
+}
+
+type Count struct {
+	Count int
+}
+
+func main() {
+
+	e := echo.New()
+
+	count := Count{Count: 0}
+
+	e.Renderer = newTemplate()
+	e.Use(middleware.Logger())
+
+	e.GET("/", func(c echo.Context) error {
+		count.Count++
+		return c.Render(200, "index.html", count)
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
